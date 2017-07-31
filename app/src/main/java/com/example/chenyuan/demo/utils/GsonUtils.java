ee1220710daa74e87bc2ad8214f655ef577e651c
chenyuan <409334552@qq.com>
2017-07-31 17:56:24 +0800

    第二次提交一些东西

---
+++ app/src/main/java/com/example/chenyuan/demo/utils/GsonUtils.java
@@ -0,0 +1,106 @@
+package com.example.chenyuan.demo.utils;
+
+import android.text.TextUtils;
+
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Yancy on 2016/4/14.
+ */
+public class GsonUtils {
+    /**
+     * 实现单例
+     */
+    private static Gson gson = null;
+
+    static {
+        if (gson == null) {
+            gson = new Gson();
+        }
+    }
+
+    /**
+     * 隐藏默认的构造方法
+     */
+    private GsonUtils() {
+
+    }
+
+    /**
+     * 将对象转换成json格式
+     *
+     * @param ts
+     * @return
+     */
+    public static String objectToJson(Object ts) {
+        String jsonStr = null;
+        if (gson != null) {
+            jsonStr = gson.toJson(ts);
+        }
+        return jsonStr;
+    }
+
+    /**
+     * 返回cla 类型的list数组
+     *
+     * @param s
+     * @param cla
+     * @return
+     */
+    public static <T extends Object> T jsonToBeanList(String s, Class<?> cla) {
+
+        List<Object> ls = new ArrayList<Object>();
+        JSONArray ss;
+        try {
+            ss = new JSONArray(s);
+            for (int i = 0; i < ss.length(); i++) {
+                String str = ss.getString(i);
+                Object a = jsonToBean(str, cla);
+                ls.add(a);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return (T) ls;
+    }
+
+
+    /**
+     * 将jsonStr转换成cl对象
+     *
+     * @param jsonStr
+     * @return
+     */
+    public static <T extends Object> T jsonToBean(String jsonStr, Class<?> cl) {
+        Object obj = null;
+        if (gson != null) {
+            if (!TextUtils.isEmpty(jsonStr))
+                obj = gson.fromJson(jsonStr, cl);
+        }
+        return (T) obj;
+    }
+
+    /**
+     * 将json格式转换成map对象
+     *
+     * @param jsonStr
+     * @return
+     */
+    public static Map<?, ?> jsonToMap(String jsonStr) {
+        Map<?, ?> objMap = null;
+        if (gson != null) {
+            java.lang.reflect.Type type = new com.google.gson.reflect.TypeToken<Map<?, ?>>() {
+            }.getType();
+            objMap = gson.fromJson(jsonStr, type);
+        }
+        return objMap;
+    }
+}
